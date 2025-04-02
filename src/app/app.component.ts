ab89d6abe2bab8c2a098a76d5cc04e1eefe902d9
Kavita2307 <kavitaavhad.237@gmail.com>
2025-04-02 13:16:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'student-registration-app';
+  // isDisplay=true;
+  buttonname='Submit';
+  student={
+  name:'',
+  age:0,
+  email:'',
+  course:''
+};
+
+  students: any[] = [];
+
+  submit() {
+    if (this.student.name && this.student.age && this.student.email && this.student.course) {
+      this.students.push({ ...this.student });
+      this.student = { name: '', age: 0, email: '' ,course:''}; // Reset form
+    }
+  }
+
+  // showHide(){
+  //   this.isDisplay=!this.isDisplay;
+  //   if(this.buttonname='Hide'){
+  //     this.buttonname='Show'
+  //   }
+  //   else{
+  //     this.buttonname='Hide';
+  //   }
+  // }
+  // students:{id:number,name:string,age:number}[]=[
+  //   {
+  //     id:101,
+  //     name:'Kavita',
+  //     age:29
+  //   },
+  //   {
+  //     id:102,
+  //     name:'Kavish',
+  //     age:5
+  //   }
+  // ]
 }
